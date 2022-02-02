41cdf05f1f41ee4a95921ffe6a0d5ee08c90cf1e
cogilvy <chrisogilvy@cox.net>
2022-02-02 14:45:27 -0800

    Add useRef hook for categories

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,12 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
 
 export default function NewOrderPage() {
   const [menuItems, setMenuItems] = useState([]);
+  const categoriesRef = useRef([]);
 
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+      categoriesRef.current = items.reduce((acc, item) => {
+        const catName = item.category.name;
+        return acc.includes(catName) ? acc : [...acc, catName];
+      }, []);
       setMenuItems(items);
     }
     getItems();
